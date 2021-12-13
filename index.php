f63b3f7296a7cfaac160def86a378010c7986524
Kelvin <fabert.kelvin@gmail.com>
2021-12-13 14:56:03 +0100

    update : home

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php require_once 'template-parts/layout/head.php' ?>
+
+<main>
+
+    <div class="container">
+        <div class="row">
+            <div class="col py-3">
+                <div class="profil">
+                    <img class="img-profil" src="https://via.placeholder.com/200" alt="">
+                        <h5 class="card-title">Coline Cabus</h5>
+                        <p class="card-text">Sportive, aime la course blabla</p>
+                </div>
+                <div class="card shadow">
+                    <h5 class="card-title">Recommandé pour vous</h5>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action">azerty</a>
+                        <a href="#" class="list-group-item list-group-item-action">azerty</a>
+                        <a href="#" class="list-group-item list-group-item-action">azerty</a>
+                        <a href="#" class="list-group-item list-group-item-action">azerty</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 py-3">
+                <div class="card mb-3 shadow">
+                    <img src="https://via.placeholder.com/100x50" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Nouvelle séance du jour</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur culpa
+                            deleniti doloribus dolorum enim fugiat iste, minus, nam optio perspiciatis recusandae rerum
+                            soluta tempore, vero voluptas. Accusantium deserunt dolore excepturi explicabo laborum,
+                            minus modi molestias nostrum obcaecati possimus saepe sint.</p>
+                        <p class="card-text"><small class="text-muted">il y a 10 min</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col py-3">
+                <div class="card shadow mb-3">
+                    <h5>Notifications récentes</h5>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action py-3">Noah Guillet</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3">Dorian Candy</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3">Kelvin Fabert</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3">Luka Sarazin</a>
+                    </div>
+                </div>
+
+                <div class="card shadow">
+                    <h5>Liste d'amis</h5>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action py-3">Noah Guillet</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3">Dorian Candy</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3">Kelvin Fabert</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3">Luka Sarazin</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
