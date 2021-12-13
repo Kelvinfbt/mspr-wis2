8e98f8dd0a0ffb0af12281a4eb50a40f66a88ab0
Cabus <coline.cabuspro@gmail.com>
2021-12-13 15:20:47 +0100

    update: SASS

---
+++ template-parts/post.php
@@ -0,0 +1,77 @@
+<?php require "../template-parts/layout/head.php" ?>
+
+<section id="section_post">
+
+    <div class="container container_post">
+        <div class="form-floating form_post w-75">
+            <h1 class="h2 text-center p-5">Ajouter un nouveau post</h1>
+            <label for="floatingTextarea2"></label>
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
+                      style="height: 100px"></textarea>
+            <div class="row row_post">
+                <div class="col-sm-6">
+                    <div class="card card_post">
+                        <div class="card-body">
+                           <img alt="" src="/assets/images/image.png"
+                                width="30px"
+                                height="30px">
+                                Photos/Vidéos
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card card_post">
+                        <div class="card-body">
+                            <img alt="" src="/assets/images/tag.png"
+                                 width="30px"
+                                 height="30px">
+                            Tagguer un ami
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card card_post">
+                        <div class="card-body">
+                            <img alt="" src="/assets/images/motivation.png"
+                                 width="30px"
+                                 height="30px">
+                            Votre motivation
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body card_post">
+                            <img alt="" src="/assets/images/lieu.png"
+                                 width="30px"
+                                 height="30px">
+                           Lieu
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body card_post">
+                            <img alt="" src="/assets/images/gif.png"
+                                 width="30px"
+                                 height="30px">
+                            Gif
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body card_post">
+                            <img alt="" src="/assets/images/smile.png"
+                                 width="30px"
+                                 height="30px">
+                            Smiley
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
