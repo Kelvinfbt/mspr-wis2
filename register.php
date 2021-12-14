94b3cc4d0fd266cb873d11400058a66adf9472ff
Noah <n.guilletgrimaud@ecoles-wis.net>
2021-12-14 14:15:26 +0100

    update: register

--- register.php
+++ register.php
@@ -14,6 +14,10 @@
                         <h1 class="text-center">S'enregistrer !</h1>
                         <form class="form">
                             <div class="mb-3">
+                                <label for="exampleInputUsername" class="form-label"></label>
+                                <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Nom d'utilisateur">
+                            </div>
+                            <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label"></label>
                                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
                             </div>
