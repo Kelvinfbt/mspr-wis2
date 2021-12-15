8d1f0811c5687b2894bd25080c8852498e8e68d6
Kelvin <fabert.kelvin@gmail.com>
2021-12-15 16:26:05 +0100

    updated : design home

--- index.php
+++ index.php
@@ -12,33 +12,36 @@ middleware('auth');
                 <?php require_once 'template-parts/layout/sidebar-menu.php'; ?>
             </div>
             <div class="col-6">
+
                 <?php require_once 'template-parts/post.php'; ?>
-                <div>
-                    <form class="d-flex py-3">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn text-light bg-dark" type="submit">Search</button>
-                    </form>
-                </div>
 
             </div>
             <div class="col">
+
+                <div>
+                    <form class="d-flex py-3">
+                        <input class="form-control me-2 shadow-sm" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn text-light bg-dark shadow-sm" type="submit">Search</button>
+                    </form>
+                </div>
+
                 <div class="notifs-list">
                     <h4 class="text-center">Notifications</h4>
-                    <div class="list-group">
-                        <a href="#" class="list-group-item list-group-item-action py-3">Noah Guillet</a>
-                        <a href="#" class="list-group-item list-group-item-action py-3">Dorian Candy</a>
-                        <a href="#" class="list-group-item list-group-item-action py-3">Kelvin Fabert</a>
-                        <a href="#" class="list-group-item list-group-item-action py-3">Luka Sarazin</a>
+                    <div class="list-group shadow-sm">
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Noah Guillet</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Dorian Candy</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Kelvin Fabert</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Luka Sarazin</a>
                     </div>
                 </div>
 
                 <div class="friends-list py-3">
                     <h4 class="text-center">Liste d'amis</h4>
-                    <div class="list-group">
-                        <a href="#" class="list-group-item list-group-item-action py-3">Noah Guillet</a>
-                        <a href="#" class="list-group-item list-group-item-action py-3">Dorian Candy</a>
-                        <a href="#" class="list-group-item list-group-item-action py-3">Kelvin Fabert</a>
-                        <a href="#" class="list-group-item list-group-item-action py-3">Luka Sarazin</a>
+                    <div class="list-group shadow-sm">
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Noah Guillet</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Dorian Candy</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Kelvin Fabert</a>
+                        <a href="#" class="list-group-item list-group-item-action py-3 border-secondary border-1">Luka Sarazin</a>
                     </div>
                 </div>
             </div>
--- template-parts/layout/sidebar-menu.php
+++ template-parts/layout/sidebar-menu.php
@@ -5,29 +5,29 @@ $auth = getAuth();
 
 <nav id="sidebar">
 
-    <ul class="list-group">
-        <li class="list-group-item">
+    <ul class="list-group shadow-sm">
+        <li class="list-group-item border-secondary border-1">
             <a href="/">
                 <div class="link-wrapper">
                     <span>Accueil</span>
                 </div>
             </a>
         </li>
-        <li class="list-group-item">
+        <li class="list-group-item border-secondary border-1">
             <a href="/notifications">
                 <div class="link-wrapper">
                     <span>Notifications</span>
                 </div>
             </a>
         </li>
-        <li class="list-group-item">
+        <li class="list-group-item border-secondary border-1">
             <a href="/messages">
                 <div class="link-wrapper">
                     <span>Messages</span>
                 </div>
             </a>
         </li>
-        <li class="list-group-item">
+        <li class="list-group-item border-secondary border-1">
             <a href="account.php">
                 <div class="link-wrapper">
                     <span>Profil</span>
@@ -35,16 +35,16 @@ $auth = getAuth();
             </a>
         </li>
         <?php if ($auth): ?>
-            <li class="nav-item list-group-item">
+            <li class="nav-item list-group-item border-secondary border-1">
                 <a class="nav-link"
                    href="/users/show.php"><?php echo $auth['username']; ?></a>
             </li>
             <?php if (isAdmin($auth)): ?>
-                <li class="nav-item list-group-item">
+                <li class="nav-item list-group-item border-secondary border-1">
                     <a class="nav-link" href="../admin">Admin</a>
                 </li>
             <?php endif; ?>
-            <li class="nav-item list-group-item">
+            <li class="nav-item list-group-item border-secondary border-1">
                 <a href="#" class="nav-link"
                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                 <form id="logout-form" action="../../api/auth/logout.php" method="POST"
