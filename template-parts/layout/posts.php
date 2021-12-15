f6901220bb5331a1d4f9d7c5c66326c205448761
Kelvin <fabert.kelvin@gmail.com>
2021-12-15 12:43:45 +0100

    update : crud

--- template-parts/layout/posts.php
+++ template-parts/layout/posts.php
@@ -1,14 +1,24 @@
+<?php
+
+require_once '../../functions/database.php';
+require_once '../../functions/helpers.php';
+require_once '../../functions/users.php';
+require_once '../../functions/post.php';
+
+$post = getPosts($_GET['id']);
+$author = getPostAuthor($post);
+
+$page = [
+        'body'=>$post['body'],
+];
+
+?>
+
 <main>
     <div class="card mb-3">
         <img src="https://via.placeholder.com/100x50" class="card-img-top" alt="">
         <div class="card-body">
-            <h5 class="card-title">Nouvelle séance du jour</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
-                culpa
-                deleniti doloribus dolorum enim fugiat iste, minus, nam optio perspiciatis recusandae
-                rerum
-                soluta tempore, vero voluptas. Accusantium deserunt dolore excepturi explicabo laborum,
-                minus modi molestias nostrum obcaecati possimus saepe sint.</p>
+           <?php echo $post['']; ?>
             <a href="">Likes</a>
             <p class="card-text"><small class="text-muted">il y a 10 min</small></p>
         </div>
