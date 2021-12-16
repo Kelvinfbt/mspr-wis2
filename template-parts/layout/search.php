787d9eedaedb0e9e4812826b168d6449129d2f59
Kelvin <fabert.kelvin@gmail.com>
2021-12-16 14:44:54 +0100

    update :

--- template-parts/layout/search.php
+++ template-parts/layout/search.php
@@ -1,8 +1,8 @@
+<?php require_once 'functions/database.php'; ?>
 
-
-<form method="POST" action="" class="d-flex py-3 droite">
-    <input class="form-control me-2 shadow-sm" type="search" placeholder="Search"
-           aria-label="Search">
-    <button class="btn btn-search" type="submit">Search</button>
-</form>
+    <form method="POST" action="../../functions/search.php" class="d-flex py-3 droite">
+        <input class="form-control me-2 shadow-sm" type="search" placeholder="Search"
+               aria-label="Search">
+        <button class="btn btn-search" type="submit">Search</button>
+    </form>
 
