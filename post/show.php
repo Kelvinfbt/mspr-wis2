f6bf03bfc178142dfb0584a5179e83dc749416fe
Noah <n.guilletgrimaud@ecoles-wis.net>
2021-12-16 10:32:23 +0100

    update:post

--- post/show.php
+++ post/show.php
@@ -24,15 +24,15 @@ $page = [
         <div class="card mb-3 mx-auto shadow mt-3">
             <img src="https://via.placeholder.com/100x50" class="card-img-top" alt="">
             <div class="card-body">
+                <button type="submit" class="btn btn-outline-danger btn-sm">
+                    like
+                </button>
                 <p><?php echo $post['body']; ?></p>
                 <form action="../api/likes/store.php" method="POST">
-                    <button type="submit" class="btn btn-outline-danger btn-sm">
-                        like
-                    </button>
                 </form>
                 <p class="card-text"><small class="text-muted">il y a 10 min</small></p>
+                <a href="api/post/edit.php?id=<?php echo $post['id'] ?>">Modifier</a>
             </div>
         </div>
     </div>
 </main>
-<a href="api/post/edit.php?id=<?php echo $post['id'] ?>">Modifier</a>
