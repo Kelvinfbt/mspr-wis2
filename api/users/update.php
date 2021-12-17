3c5f61ac7ce2298f1c6f6e576588f78ec7b7b143
Cabus <coline.cabuspro@gmail.com>
2021-12-17 09:10:01 +0100

    update: update

--- api/users/update.php
+++ api/users/update.php
@@ -4,28 +4,41 @@ require_once '../../functions/helpers.php';
 middleware('auth');
 
 $username = $_POST['username'];
-$prenom = $_POST['first_name'];
-$nom = $_POST['last_name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$about = $_POST['about'];
+$sport = $_POST['sport'];
+$lieu = $_POST['lieu'];
+$niveau = $_POST['niveau'];
+$salle = $_POST['salle'];
 $id = $_GET['id'];
 
 $connexion = new PDO('mysql:host=localhost:8889;dbname=msprwis2', 'root', 'root');
 
-$query = $connexion->prepare("UPDATE users SET username = :username, first_name = :first_name, last_name = :last_name, email = :email, password = :password WHERE id = :id");
+$query = $connexion->prepare("UPDATE users SET username = :username, email = :email, password = :password WHERE id = :id");
 
+$stmt = $connexion->prepare('INSERT INTO users (about, sport, lieu, niveau, salle) VALUES (:about, :sport, :lieu, :niveau, :salle)');
+$stmt->bindParam(':about', $data['about']);
+$stmt->bindParam(':sport', $data['sport']);
+$stmt->bindParam(':lieu', $data['lieu']);
+$stmt->bindParam(':niveau', $data['niveau']);
+$stmt->bindParam(':salle', $data['salle']);
+$stmt->execute();
 
 $values = [
     'username' => $username,
-    'first_name' => $prenom,
-    'last_name' => $nom,
     'email' => $email,
     'password' => $password,
+    'about' => $about,
+    'sport' => $sport,
+    'lieu' => $lieu,
+    'niveau' => $niveau,
+    'salle' => $salle,
     'id' => $id,
 ];
 
 $query->execute($values);
 
-header('Location:../../users/index.php');
+header('Location:../../account.php');
 
 ?>
--- users/edit.php
+++ users/edit.php
@@ -1,25 +1,83 @@
-<?php require $_SERVER['DOCUMENT_ROOT'] . "/template-parts/layout/header.php";
+<?php
+require_once "../template-parts/layout/head.php";
+require_once '../functions/database.php';
+require_once '../functions/helpers.php';
+require_once '../functions/users.php';
 
 middleware('auth');
 
+
+
 $auth = getAuth(); ?>
 
-<main>
+    <main>
 
-    <section id="user-edit" class="py-5">
-        <div class="container">
+        <section id="user-edit" class="py-5">
+            <div class="container">
 
-            <h1>Modifier le profil de <?php echo $auth['username']; ?></h1>
+                <h1>Modifier le profil de <?php echo $auth['username']; ?></h1>
 
-            <form action="api/users/store.php" method="POST">
+                <form action="/api/users/update.php" method="POST">
 
-                // FORM
 
-            </form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">username</label>
+                        <input id="username" name="username" type="text" class="form-control" placeholder="Username" aria-label="username"
+                               value="<?= $auth['username'] ?>">
+                    </div>
 
-        </div>
-    </section>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                               value="<?= $auth['email'] ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input name="password" type="password" class="form-control" id="password" value="<?= $auth['password'] ?>">
+                    </div>
 
-</main>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">About</label>
+                        <input id="about" name="about" type="text" class="form-control" placeholder="About" aria-label="about"
+                               value="">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">sport</label>
+                        <input id="sport" name="sport" type="text" class="form-control" placeholder="Sport" aria-label="sport"
+                               value="">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Lieu</label>
+                        <input id="lieu" name="lieu" type="text" class="form-control" placeholder="Lieu" aria-label="lieu"
+                               value="">
+                    </div>
+
+                    <select name="niveau" class="form-select" aria-label="Default select example">
+                        <option selected>Niveau sportif</option>
+                        <option value="0">Débutant</option>
+                        <option value="1">Intermédiaire</option>
+                        <option value="1">Confirmé</option>
+                        <option value="1">Expert</option>
+                    </select>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Salle de sport</label>
+                        <input id="salle" name="salle" type="text" class="form-control" placeholder="Salle" aria-label="salle"
+                               value="">
+                    </div>
+
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+
+
+
+            </div>
+        </section>
+
+    </main>
 
 <?php require $_SERVER['DOCUMENT_ROOT'] . "/template-parts/layout/footer.php"; ?>
